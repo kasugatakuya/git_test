27c543da3afb0a4a9824ddbd7aaa138f0d55c2f9
kasugatakuya <rxsgw123@yahoo.co.jp>
2022-02-05 13:01:07 +0900

    git_test-selectボックス追加
    git_test-selectボックス追加

--- post.php
+++ post.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="js">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>post</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
 </head>
 <body>
-  <h1><?php echo $_POST['text']?></h1>
-  <h2><?php echo $_POST['color']?></h2>
+    <h1><?php echo $_POST['text']?></h1>
+    <h2><?php echo $_POST['color']?></h2>
+    <h3><?php echo $_POST['example']?></h3>
 </body>
 </html>
